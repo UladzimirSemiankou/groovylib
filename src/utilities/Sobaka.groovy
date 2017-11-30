d48d40d3a8c6ab10072ac69c8793dd746dd47b64
Vladimir Semenkov <Uladzimir_Semiankou@epam.com>
2017-11-30 17:12:21 +0300

    Update Sobaka.groovy

--- src/utilities/Sobaka.groovy
+++ src/utilities/Sobaka.groovy
@@ -3,6 +3,6 @@ package utilities
 class Utilities {
   static def classshare(script, var) {
     //script.echo "i am shared class method. Var is ${var}"
-    println "i am shared class method too. Var is ${var}"
+    script.println "i am shared class method too. Var is ${var}"
   }
 }
