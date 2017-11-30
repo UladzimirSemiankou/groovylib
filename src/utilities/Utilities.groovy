2fd8fa146eb6244b9534afc1ba14fdf5a92f0dc0
Vladimir Semenkov <Uladzimir_Semiankou@epam.com>
2017-11-30 17:00:48 +0300

    Create Utilities.groovy

---
+++ src/utilities/Utilities.groovy
@@ -0,0 +1,8 @@
+package utilities
+
+class Utilities {
+  static def classshare(var) {
+    echo "i am shared class method. Var is ${var}"
+    println "i am shared class method too. Var is ${var}"
+  }
+}
