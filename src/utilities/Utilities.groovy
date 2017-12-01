9088e3282a1598492c61af76d8f8b0bbb42a4028
Vladimir Semenkov <Uladzimir_Semiankou@epam.com>
2017-12-01 14:11:59 +0300

    Update Utilities.groovy

--- src/utilities/Utilities.groovy
+++ src/utilities/Utilities.groovy
@@ -4,6 +4,6 @@ class Utilities {
   static def classshare(script, var) {
     script.echo "i am shared class method. Var is ${var}"
     script.println "i am shared class method too. Var is ${var}"
-    varshare 'huiz'
+    script.varshare 'huiz'
   }
 }
