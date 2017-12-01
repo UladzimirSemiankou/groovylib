c0ee37f95623de0a88817ca5231c5052f2c4a823
Vladimir Semenkov <Uladzimir_Semiankou@epam.com>
2017-12-01 17:05:30 +0300

    Create logger.groovy

---
+++ vars/logger.groovy
@@ -0,0 +1,13 @@
+enum Level {DEBUG, INFO}
+
+def debug(message) {
+  if (env.LogLevel <= Level.DEBUG) {
+    println '[DEBUG]: ${message}'
+  }
+}
+
+def info(message) {
+  if (env.LogLevel <= Level.INFO) {
+    println '[INFO]: ${message}'
+  }
+}
