1191f5795d4c925a6e31d454a5c9d86ca20cf9c5
Vladimir Semenkov <Uladzimir_Semiankou@epam.com>
2017-12-01 17:07:10 +0300

    Update logger.groovy

--- vars/logger.groovy
+++ vars/logger.groovy
@@ -2,12 +2,12 @@ enum Level {DEBUG, INFO}
 
 def debug(message) {
   if (env.LogLevel <= Level.DEBUG) {
-    println '[DEBUG]: ${message}'
+    println "[DEBUG]: ${message}"
   }
 }
 
 def info(message) {
   if (env.LogLevel <= Level.INFO) {
-    println '[INFO]: ${message}'
+    println "[INFO]: ${message}"
   }
 }
